65bf278555bfe1c846f93abd29f00df762ddf8a9
weiii1 <87006954+weiii1@users.noreply.github.com>
2021-07-20 15:29:28 +0800

    Update app.py

--- app.py
+++ app.py
@@ -53,8 +53,11 @@ def callback():
 ##### 基本上程式編輯都在這個function #####
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text=event.message.text)
-    line_bot_api.reply_message(event.reply_token,message)
+    message = text=event.message.text
+    if re.match('告訴我秘密',message):
+        line_bot_api.reply_message(event.reply_token,TextSendMessage('才不告訴你哩！'))
+    else:
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(message))
 
 #主程式
 import os
