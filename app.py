abb7ca62ad08e516e6ba02b9b68e46577594c05c
weiii1 <87006954+weiii1@users.noreply.github.com>
2021-07-27 16:50:56 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -53,27 +53,62 @@ def callback():
 ##### 基本上程式編輯都在這個function #####
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = text=event.message.text
-    if re.match('告訴我秘密',message):
-         confirm_template_message = TemplateSendMessage(
-             alt_text='問問題',
-             template=ConfirmTemplate(
-                 text='你喜這堂課嗎？',
-                 actions=[
-                     PostbackAction(
-                         label='喜歡',
-                         display_text='超喜歡',
-                         data='action=其實不喜歡'
+     message = text=event.message.text
+     if re.match('告訴我秘密',message):
+         carousel_template_message = TemplateSendMessage(
+             alt_text='免費教學影片',
+             template=CarouselTemplate(
+                 columns=[
+                     CarouselColumn(
+                         thumbnail_image_url='https://i.imgur.com/wpM584d.jpg',
+                         title='Python基礎教學',
+                         text='萬丈高樓平地起',
+                         actions=[
+                             MessageAction(
+                                 label='教學內容',
+                                 text='拆解步驟詳細介紹安裝並使用Anaconda、Python、Spyder、VScode…'
+                             ),
+                             URIAction(
+                                 label='馬上查看',
+                                 uri='https://marketingliveincode.com/?page_id=270'
+                             )
+                         ]
                      ),
-                     MessageAction(
-                         label='愛',
-                         text='愛愛'
+                     CarouselColumn(
+                         thumbnail_image_url='https://i.imgur.com/W7nI6fg.jpg',
+                         title='Line Bot聊天機器人',
+                         text='台灣最廣泛使用的通訊軟體',
+                         actions=[
+                             MessageAction(
+                                 label='教學內容',
+                                 text='Line Bot申請與串接'
+                             ),
+                             URIAction(
+                                 label='馬上查看',
+                                 uri='https://marketingliveincode.com/?page_id=2532'
+                             )
+                         ]
+                     ),
+                     CarouselColumn(
+                         thumbnail_image_url='https://i.imgur.com/l7rzfIK.jpg',
+                         title='Telegram Bot聊天機器人',
+                         text='唯有真正的方便，能帶來意想不到的價值',
+                         actions=[
+                             MessageAction(
+                                 label='教學內容',
+                                 text='Telegrame申請與串接'
+                             ),
+                             URIAction(
+                                 label='馬上查看',
+                                 uri='https://marketingliveincode.com/?page_id=2648'
+                             )
+                         ]
                      )
                  ]
              )
          )
-         line_bot_api.reply_message(event.reply_token, confirm_template_message)
-    else:
+         line_bot_api.reply_message(event.reply_token, carousel_template_message)
+     else:
          line_bot_api.reply_message(event.reply_token, TextSendMessage(message))
 #主程式
 import os
