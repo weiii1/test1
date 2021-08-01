14a922b06505a137ab33a4faa947c4643953a86e
weiii1 <87006954+weiii1@users.noreply.github.com>
2021-08-01 14:56:00 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -53,50 +53,63 @@ def callback():
 ##### 基本上程式編輯都在這個function #####
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    resp = requests.get('https://tw.rter.info/capi.php')
-    currency_data = resp.json()
-    usd_to_twd = currency_data['USDTWD']['Exrate']
-    usd_to_jpy = currency_data['USDJPY']['Exrate']
-    usd_to_eur = currency_data['USDEUR']['Exrate']
-    usdtwd=float(usd_to_twd)
-    usdjpy=float(usd_to_jpy)
-    usdeur=float(usd_to_eur)
-    twdjpy=usdjpy/usdtwd
-    twdeur=usdeur/usdtwd
-    message = text=event.message.text
-    if re.match('匯率',message):
-        buttons_template_message = TemplateSendMessage(
-         alt_text='這個看不到',
-         template=ButtonsTemplate(
-             thumbnail_image_url='https://i.imgur.com/wpM584d.jpg',title='請選擇幣別',text='幣別',
-             actions=[
-                  MessageAction(
-                     label='美元',
-                     text='美元匯率'
-                 ),
-                 MessageAction(
-                     label='歐元',
-                     text='歐元匯率'
-                 ),
-                 MessageAction(
-                     label='日元',
-                     text='日元匯率'
-                 ),
-             ]
+     message = text=event.message.text
+     if re.match('告訴我秘密',message):
+         carousel_template_message = TemplateSendMessage(
+             alt_text='免費教學影片',
+             template=CarouselTemplate(
+                 columns=[
+                     CarouselColumn(
+                         thumbnail_image_url='https://i.imgur.com/wpM584d.jpg',
+                         title='Python基礎教學',
+                         text='萬丈高樓平地起',
+                         actions=[
+                             MessageAction(
+                                 label='教學內容',
+                                 text='拆解步驟詳細介紹安裝並使用Anaconda、Python、Spyder、VScode…'
+                             ),
+                             URIAction(
+                                 label='馬上查看',
+                                 uri='https://marketingliveincode.com/?page_id=270'
+                             )
+                         ]
+                     ),
+                     CarouselColumn(
+                         thumbnail_image_url='https://i.imgur.com/W7nI6fg.jpg',
+                         title='Line Bot聊天機器人',
+                         text='台灣最廣泛使用的通訊軟體',
+                         actions=[
+                             MessageAction(
+                                 label='教學內容',
+                                 text='Line Bot申請與串接'
+                             ),
+                             URIAction(
+                                 label='馬上查看',
+                                 uri='https://marketingliveincode.com/?page_id=2532'
+                             )
+                         ]
+                     ),
+                     CarouselColumn(
+                         thumbnail_image_url='https://i.imgur.com/l7rzfIK.jpg',
+                         title='Telegram Bot聊天機器人',
+                         text='唯有真正的方便，能帶來意想不到的價值',
+                         actions=[
+                             MessageAction(
+                                 label='教學內容',
+                                 text='Telegrame申請與串接'
+                             ),
+                             URIAction(
+                                 label='馬上查看',
+                                 uri='https://marketingliveincode.com/?page_id=2648'
+                             )
+                         ]
+                     )
+                 ]
+             )
          )
-     )
-        line_bot_api.reply_message(event.reply_token, buttons_template_message)
-    elif re.match('美元匯率',message):
-        line_bot_api.reply_message(event.reply_token,TextSendMessage(text=f'美元 USD 對台幣 TWD：1:{usd_to_twd}'))
-    elif re.match('歐元匯率',message):
-        line_bot_api.reply_message(event.reply_token,TextSendMessage(text=f'台幣 TWD 對歐元 EUR：1:{twdeur}'))
-    elif re.match('日元匯率',message):
-        line_bot_api.reply_message(event.reply_token,TextSendMessage(text=f'台幣 TWD 對日幣 JPY：1:{twdjpy}'))
-    #以上為匯率功能#以上為匯率功能#以上為匯率功能#以上為匯率功能#以上為匯率功能#以上為匯率功能
-    elif re.match('記帳',message):
-        line_bot_api.reply_message(event.reply_token,TextSendMessage('老子懶的記'))
-    else:
-        line_bot_api.reply_message(event.reply_token, TextSendMessage('沒有此指令'))
+         line_bot_api.reply_message(event.reply_token, carousel_template_message)
+     else:
+         line_bot_api.reply_message(event.reply_token, TextSendMessage(message))
 #主程式
 import os
 if __name__ == "__main__":
